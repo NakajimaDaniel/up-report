12edb1a138b92d5fd9d76acea5d5a5af977eb414
NakajimaDaniel <nakajimadaniel96@gmail.com>
2022-02-25 19:25:52 -0300

    chakraui config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
+import type { AppProps } from 'next/app';
+
+const theme = extendTheme({
+  styles: {
+    global: (props: any) => ({
+      body: {
+        fontFamily: 'body',
+        color: mode('gray.800', 'whiteAlpha.900')(props),
+        bg: mode('white', 'gray.700')(props),
+        lineHeight: 'base',
+      },
+    }),
+  },
+  fonts: {
+    heading: 'DM Sans, sans-serif',
+    body: 'DM Sans, sans-serif',
+  },
+
+})
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
