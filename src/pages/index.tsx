86824295190e6e11dd080339305db507edc2fd90
NakajimaDaniel <nakajimadaniel96@gmail.com>
2022-02-26 19:37:42 -0300

    feat: front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
-
+import { Box, Text, Center, Image } from '@chakra-ui/react';
 
 export default function Home() {
 
   return (
-    <div>Home</div>
+    <Center h="100vh" maxW="100vw" w="100%" bg="#46BCFF">
+      <Box>
+        <Text color="white" fontWeight="bold" fontSize="30px" textAlign="center">UpReport</Text>
+        <Text color="white" fontSize="15px" textAlign="center" pb="10"  >keep track on your expenses</Text>
+        <Center w="270px" h="50px" bg="#EEEEEE" sx={{ borderRadius: '10px', 'transition': '0.3s' }} _hover={{ cursor: 'pointer', backgroundColor: '#e2e2e2' }} >
+          <Image src="google-icon.svg" />
+          <Text color="#525252" pl="5" fontWeight="regular">Login with Google</Text>
+        </Center>
+      </Box>
+    </Center>
   )
 
 }
