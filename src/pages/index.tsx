98ab485f5b1fe1c82e072f54ebfe50a100218a5a
NakajimaDaniel <nakajimadaniel96@gmail.com>
2022-03-01 18:59:18 -0300

    fix: if user already exist redirect to dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,30 @@
 import { Box, Text, Center, Image } from '@chakra-ui/react';
-import { useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useContext, useEffect } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 
 
 
 export default function Home() {
 
-  const { signInWithGoogle } = useContext(AuthContext);
+  const { signInWithGoogle, user } = useContext(AuthContext);
+
+  const router = useRouter();
+
+  function handleLoginWithGoogleButton() {
+    if (!user) {
+      signInWithGoogle()
+    } else {
+      router.push('/dashboard')
+    }
+  }
 
   return (
     <Center h="100vh" maxW="100vw" w="100%" bg="#46BCFF">
       <Box>
         <Text color="white" fontWeight="bold" fontSize="30px" textAlign="center">UpReport</Text>
         <Text color="white" fontSize="15px" textAlign="center" pb="10"  >keep track on your expenses</Text>
-        <Center w="270px" h="50px" bg="#EEEEEE" sx={{ borderRadius: '10px', 'transition': '0.3s' }} _hover={{ cursor: 'pointer', backgroundColor: '#e2e2e2' }} as="button" onClick={signInWithGoogle}  >
+        <Center w="270px" h="50px" bg="#EEEEEE" sx={{ borderRadius: '10px', 'transition': '0.3s' }} _hover={{ cursor: 'pointer', backgroundColor: '#e2e2e2' }} as="button" onClick={handleLoginWithGoogleButton}  >
           <Image src="google-icon.svg" />
           <Text color="#525252" pl="5" fontWeight="regular">Login with Google</Text>
         </Center>
