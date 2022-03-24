0c290a8c918289cd9d8ef7659b5fb31964f7afbd
NakajimaDaniel <nakajimadaniel96@gmail.com>
2022-03-24 19:28:11 -0300

    feat: latest expense card component

--- src/components/LatestExpensesCard/index.tsx
+++ src/components/LatestExpensesCard/index.tsx
@@ -1,7 +1,38 @@
 import { Container, Flex, Spacer, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { format } from "date-fns";
 
 
-export function LatestExpensesCard() {
+
+interface Transaction {
+  dt: number,
+  description: string,
+  category: string,
+  type: string,
+  value: string,
+}
+
+interface LatestExpensesCardProps {
+  transactions: Transaction[];
+}
+
+
+export function LatestExpensesCard({ transactions }: LatestExpensesCardProps) {
+
+  const allExpenseTransactions = transactions?.filter(val => {
+    if (val.type == "Expense") return val.category
+  })
+
+
+  const newTransactionsList = allExpenseTransactions?.map(val => {
+    return {
+      dt: val.dt,
+      description: val.description,
+      category: val.category,
+      value: Number(val.value).toFixed(2)
+    }
+  });
+
+
   return (
     <Container w="100%" bg="#364154" borderRadius="10px" pl={6} pr={6} pt={6}>
       <Text fontWeight="semibold" fontSize={19} pb={4}>Latest Expenses</Text>
@@ -17,19 +48,18 @@ export function LatestExpensesCard() {
         </Thead>
 
         <Tbody>
-          <Tr>
-            <Td>02/02/22</Td>
-            <Td>Bluetooth Adapter</Td>
-            <Td>Electronics</Td>
-            <Td color="#EB4335">R$ -50,00</Td>
-          </Tr>
-
-          <Tr>
-            <Td>02/02/22</Td>
-            <Td>Toothbrush</Td>
-            <Td>Personal Care</Td>
-            <Td color="#EB4335">R$ -15,00</Td>
-          </Tr>
+          {transactions ? (
+            newTransactionsList.map(val => {
+              return (
+                <Tr key={val.dt}>
+                  <Td>{format(val.dt, "dd/MM/yyyy")}</Td>
+                  <Td>{val.description}</Td>
+                  <Td>{val.category}</Td>
+                  <Td color="#EB4335">R$ -{val.value}</Td>
+                </Tr>
+              )
+            })
+          ) : (<Tr></Tr>)}
         </Tbody>
       </Table>
 
