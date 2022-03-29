0ee5e6713d41c0a9bf5e22977c3e9759d071b43f
NakajimaDaniel <nakajimadaniel96@gmail.com>
2022-03-29 19:03:22 -0300

    feat: added all data from firebase

--- src/components/AllExpensesPerMonthTable/index.tsx
+++ src/components/AllExpensesPerMonthTable/index.tsx
@@ -1,59 +1,136 @@
-import { Container, Select, Table, Tbody, Td, Text, Tfoot, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Container, Select, Table, Tbody, Td, Text, Tfoot, Th, Thead, Tr } from "@chakra-ui/react";
+import { format } from "date-fns";
+import { useEffect, useState } from "react";
+
+interface Transaction {
+  dt: number,
+  description: string,
+  category: string,
+  type: string,
+  value: string,
+}
+
+interface AllExpensesPerMonthTableProps {
+  transactions: Transaction[];
+}
+
+export function AllExpensesPerMonthTable({ transactions }: AllExpensesPerMonthTableProps) {
+
+  const [selectedMonthYear, setSelectedMonthYear] = useState();
+  const [selectedMonthYearTransactions, setSelectedMonthYearTransaction] = useState();
+  const [totalExpenseValue, setTotalExpenseValue] = useState();
+  const [totalIncomeValue, setTotalIncomeValue] = useState();
+
+  const newTransactionList = transactions?.map(val => {
+    return {
+      dt: val.dt,
+      category: val.category,
+      description: val.description,
+      type: val.type,
+      value: val.value,
+      monthYear: format(val.dt, "MMMM/yy"),
+    }
+  });
+
+  const listOfMonthYear = newTransactionList?.map(val => { return val.monthYear });
+
+  const listOfMonthYearFiltered = listOfMonthYear?.filter((item, position) => {
+    return listOfMonthYear.indexOf(item) == position
+  })
+
+  function selectMonthTransactions(selectedMonthYear, transactions) {
+    const transactionList = transactions?.filter(val => {
+      if (val.monthYear == selectedMonthYear) {
+        return val
+      }
+    })
+
+    return transactionList
+  }
+
+  useEffect(() => {
+    const newTransactionArray = selectMonthTransactions(selectedMonthYear, newTransactionList);
+    setSelectedMonthYearTransaction(newTransactionArray);
+
+  }, [selectedMonthYear]);
+
+  useEffect(() => {
+    const allExpenses = selectedMonthYearTransactions?.filter(val => { return val.type == "Expense" });
+    const allIncomes = selectedMonthYearTransactions?.filter(val => { return val.type == "Income" });
+
+    const allExpensesFormatted = allExpenses?.map(val => {
+      return {
+        value: Number(val.value)
+      }
+    })
+
+    const allIncomesFormatted = allIncomes?.map(val => {
+      return {
+        value: Number(val.value)
+      }
+    })
+
+    const totalExpensesSum = allExpensesFormatted?.reduce((total, transaction) => {
+      return total = total + transaction.value
+    }, 0)
+
+    const totalIncomesSum = allIncomesFormatted?.reduce((total, transaction) => {
+      return total = total + transaction.value
+    }, 0)
 
 
+    setTotalIncomeValue(totalIncomesSum);
+    setTotalExpenseValue(totalExpensesSum);
 
-export function AllExpensesPerMonthTable() {
+  }, [selectedMonthYearTransactions])
+
   return (
     <Container w="100%" bg="#364154" borderRadius="10px" pl={6} pr={6} pt={6}>
       <Text fontWeight="semibold" fontSize={19} pb={4}>All Expenses per Month </Text>
 
-      <Select placeholder='Select option' pb={5}>
-        <option value='option1'>January</option>
-        <option value='option2'>February</option>
-        <option value='option3'>March</option>
+      <Select placeholder='Select option' mb={5} onClick={(e) => setSelectedMonthYear(e.target.value)} >
+        {listOfMonthYearFiltered?.map(val => {
+          return (
+            <option key={val} value={val}>{val}</option>
+          )
+        })}
       </Select>
 
-      <Table mb={4} colorScheme="whiteAlpha">
-        <Thead>
-          <Tr>
-            <Th>Date</Th>
-            <Th>Item</Th>
-            <Th>Category</Th>
-            <Th>Value</Th>
-          </Tr>
-        </Thead>
+      <Box overflowX="auto" >
+        <Table mb={4} colorScheme="whiteAlpha">
+          <Thead>
+            <Tr>
+              <Th>Date</Th>
+              <Th>Item</Th>
+              <Th>Category</Th>
+              <Th>Value</Th>
+            </Tr>
+          </Thead>
 
-        <Tbody>
-          <Tr>
-            <Td>02/02/22</Td>
-            <Td>Bluetooth Adapter</Td>
-            <Td>Electronics</Td>
-            <Td color="#EB4335">R$ -50,00</Td>
-          </Tr>
+          <Tbody>
+            {selectedMonthYearTransactions?.map(val => {
+              return (
+                <Tr key={val.dt} >
+                  <Td>{format(val.dt, "dd/MM/yy")}</Td>
+                  <Td>{val.description}</Td>
+                  <Td>{val.category}</Td>
+                  <Td color={val.type == "Expense" ? "#EB4335" : "#5FF099"}>R$ {val.type == "Expense" ? "-" : ""} {Number(val.value)}</Td>
+                </Tr>
+              )
+            })}
 
-          <Tr>
-            <Td>02/02/22</Td>
-            <Td>Toothbrush</Td>
-            <Td>Personal Care</Td>
-            <Td color="#EB4335">R$ -15,00</Td>
-          </Tr>
-          <Tr>
-            <Td>02/02/22</Td>
-            <Td>Salary</Td>
-            <Td></Td>
-            <Td color="#5FF099">R$ 500,00</Td>
-          </Tr>
-        </Tbody>
+          </Tbody>
 
-        <Tfoot>
-          <Tr>
-            <Td></Td>
-            <Td></Td>
-            <Td></Td>
-            <Td color="#5FF099">R$ 500,00</Td>
-          </Tr>
-        </Tfoot>
-      </Table>
+          <Tfoot>
+            <Tr>
+              <Td></Td>
+              <Td></Td>
+              <Td></Td>
+              <Td color={totalExpenseValue > totalIncomeValue ? "#EB4335" : "#5FF099"}>R$ {totalIncomeValue - totalExpenseValue}</Td>
+            </Tr>
+          </Tfoot>
+        </Table>
+      </Box>
 
     </Container>
   )
