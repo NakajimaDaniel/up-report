b7215000a8effd241d0e6620449b3a469931d3cc
NakajimaDaniel <nakajimadaniel96@gmail.com>
2022-03-31 19:49:30 -0300

    feat: added logout function to mobile display

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,30 @@
-import { Box, Button, Container, Flex, Image, MenuButton, MenuItem, MenuList, Skeleton, Spacer, Text, Menu as ChakraMenu, Icon } from "@chakra-ui/react";
+import { Box, Button, Container, Flex, Image, MenuButton, MenuItem, MenuList, Skeleton, Spacer, Text, Menu as ChakraMenu, Icon, Popover, PopoverTrigger, PopoverContent, PopoverArrow, PopoverHeader, PopoverBody } from "@chakra-ui/react";
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 import { useDimensions } from "../../hooks/useDimensions";
 import { Menu } from '../Menu';
 import { Menu as FeatherMenu } from 'react-feather';
 import Link from "next/link";
+import { getAuth, signOut } from "../../services/firebase";
+import { useRouter } from "next/router";
 
 export function Header() {
 
   const { user } = useContext(AuthContext);
+  const router = useRouter()
 
   const { width } = useDimensions();
+
+  function handleLogout() {
+    const auth = getAuth();
+
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      router.push("/")
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
 
   return (
     <Container w="100%" maxW="100vw" p={[5]}>
@@ -30,7 +44,7 @@ export function Header() {
           </Skeleton>
         </Flex>
       ) : (
-        <Flex align="center" >
+        <Flex align="center" pl={5}>
           <Text color="white" fontWeight="bold" fontSize="30px" textAlign="center">UpReport</Text>
           <Spacer />
           <ChakraMenu>
@@ -55,6 +69,22 @@ export function Header() {
               </Link>
             </MenuList>
           </ChakraMenu>
+
+          <Popover>
+            <PopoverTrigger>
+              <Skeleton isLoaded={!!user} startColor='pink.500' endColor='orange.500' _hover={{ cursor: "pointer" }} ml={5} >
+                <Image src={user?.photoURL} width="40px" height="40px" mr={3} />
+              </Skeleton>
+            </PopoverTrigger>
+            <PopoverContent>
+              <PopoverArrow />
+              <PopoverHeader>Signed in as {user?.displayName}</PopoverHeader>
+              <PopoverBody>
+                <Button onClick={() => handleLogout()} >Sign out</Button>
+              </PopoverBody>
+            </PopoverContent>
+          </Popover>
+
         </Flex>
 
       )}
