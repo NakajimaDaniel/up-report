68313036a4992c69b139cb5e513be442a5d5ca4a
NakajimaDaniel <nakajimadaniel96@gmail.com>
2022-04-01 19:24:03 -0300

    fix: validate if user is signed in and adjusted responsiveness

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,11 +1,12 @@
 import { Container, HStack, VStack } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { LatestExpensesCard } from "../../components/LatestExpensesCard";
-import { Menu } from "../../components/Menu";
 import { MonthBalanceCard } from "../../components/MonthBalanceCard";
 import { TotalExpenseByCategoryCard } from "../../components/TotalExpenseByCategoryCard";
 import { AuthContext } from "../../contexts/AuthContext";
+import { useDimensions } from "../../hooks/useDimensions";
 import { ref, get, child, getDatabase } from "../../services/firebase";
 
 
@@ -20,31 +21,53 @@ interface Transaction {
 export default function Dashboard() {
 
   const { user } = useContext(AuthContext);
+  const { width } = useDimensions();
+  const router = useRouter();
 
   const [listOfTransactions, setListOfTransactions] = useState<Transaction[]>();
 
   useEffect(() => {
-    const dbRef = ref(getDatabase());
-    get(child(dbRef, `users/transactions`)).then((snapshot) => {
-      if (snapshot.exists()) {
-        const data = snapshot.val();
-        const result = Object.entries(data).map(([key, value]) => { return value });
-        setListOfTransactions(result);
-      }
-    })
+
+    if (user) {
+      const dbRef = ref(getDatabase());
+      get(child(dbRef, `users`)).then((snapshot) => {
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          const result = Object.entries(data).map(([key, value]) => { return key });
+        }
+      })
+
+
+      get(child(dbRef, `users/${user.uid}/transactions`)).then((snapshot) => {
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          const result = Object.entries(data).map(([key, value]) => { return value });
+          setListOfTransactions(result);
+        }
+      })
+    } else {
+      router.push('/')
+    }
+
+
   }, [])
 
 
   return (
     <Container w="100%" maxW="100vw" >
       <Header />
-
-      <HStack mr={["200px", "200px", "250px", "300px"]} ml={["200px", "200px", "250px", "300px"]} pb={10} alignItems="center">
-        <MonthBalanceCard user={user} transactions={listOfTransactions} />
-        <TotalExpenseByCategoryCard transactions={listOfTransactions} />
-      </HStack>
-
-      <VStack mr={["200px", "200px", "250px", "300px"]} ml={["200px", "200px", "250px", "300px"]} pb={10} alignItems="center">
+      {width && width > 660 ? (
+        <HStack mr={width && width > 1180 ? ["200px", "200px", "250px", "300px"] : [0]} ml={width && width > 1180 ? ["200px", "200px", "250px", "300px"] : [0]} pb={10} alignItems="center">
+          <MonthBalanceCard user={user} transactions={listOfTransactions} />
+          <TotalExpenseByCategoryCard transactions={listOfTransactions} />
+        </HStack>
+      ) : (
+        <VStack mb={5}>
+          <MonthBalanceCard user={user} transactions={listOfTransactions} />
+          <TotalExpenseByCategoryCard transactions={listOfTransactions} />
+        </VStack>
+      )}
+      <VStack mr={width && width > 1180 ? ["200px", "200px", "250px", "300px"] : [0]} ml={width && width > 1180 ? ["200px", "200px", "250px", "300px"] : [0]} pb={10} alignItems="center">
         <LatestExpensesCard transactions={listOfTransactions} />
       </VStack>
 
