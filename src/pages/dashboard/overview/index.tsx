d811041d8425b23c68159d8d76ac36b585f76208
NakajimaDaniel <nakajimadaniel96@gmail.com>
2022-04-01 19:25:19 -0300

    fix: get only data from authenticated user

--- src/pages/dashboard/overview/index.tsx
+++ src/pages/dashboard/overview/index.tsx
@@ -1,9 +1,10 @@
 import { Box, Container, Text, VStack } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AllExpensesPerMonthTable } from "../../../components/AllExpensesPerMonthTable";
 import { Header } from "../../../components/Header";
 import { Menu } from "../../../components/Menu";
 import { MonthlyBalanceGraph } from "../../../components/MonthlyBalanceGraph";
+import { AuthContext } from "../../../contexts/AuthContext";
 import { get, ref, getDatabase, child } from '../../../services/firebase';
 
 
@@ -20,10 +21,11 @@ export default function Overview() {
 
 
   const [listOfTransactions, setListOfTransactions] = useState<Transaction[]>();
+  const { user } = useContext(AuthContext);
 
   useEffect(() => {
     const dbRef = ref(getDatabase());
-    get(child(dbRef, `users/transactions`)).then((snapshot) => {
+    get(child(dbRef, `users/${user?.uid}/transactions`)).then((snapshot) => {
       if (snapshot.exists()) {
         const data = snapshot.val();
         const result = Object.entries(data).map(([key, value]) => { return value });
