a874028345000aeae4759610b8e381b7c31000de
NakajimaDaniel <nakajimadaniel96@gmail.com>
2022-04-11 19:41:16 -0300

    fix: types fix

--- src/components/MonthBalanceCard/index.tsx
+++ src/components/MonthBalanceCard/index.tsx
@@ -1,12 +1,6 @@
 import { Container, Divider, Flex, Skeleton, Spacer, Text } from "@chakra-ui/react";
 import { format } from "date-fns";
-
-interface User {
-  uid: string,
-  displayName: string | null,
-  email: string | null,
-  photoURL: string,
-}
+import { useEffect, useState } from "react";
 
 interface Transaction {
   dt: number,
@@ -17,44 +11,67 @@ interface Transaction {
 }
 
 interface MonthBalanceCardProps {
-  user: User;
-  transactions: Transaction[];
+  transactions: Transaction[] | undefined;
+}
+
+interface TotalBalance {
+  value: string,
+  type: string,
 }
 
 
-export function MonthBalanceCard({ user, transactions }: MonthBalanceCardProps) {
+export function MonthBalanceCard({ transactions }: MonthBalanceCardProps) {
+
+  const [totalExpense, setTotalExpense] = useState<string>();
+  const [totalIncome, setTotalIncome] = useState<string>();
+  const [totalBalance, setTotalBalance] = useState<TotalBalance>();
 
   const currentMonth = format(new Date(), "MMMM");
   const currentYear = format(new Date(), "yyyy");
 
+  useEffect(() => {
+    if (transactions) {
+      const transactionsFormatted = transactions.map(val => (
+        {
+          dt: val.dt,
+          description: val.description,
+          category: val.category,
+          type: val.type,
+          value: Number(val.value),
+          monthRegistered: format(val.dt, "MMMM"),
+          yearRegistered: format(val.dt, "yyyy"),
+        }
+      ))
 
-  const transactionsFormatted = transactions?.map(val => (
-    {
-      dt: val.dt,
-      description: val.description,
-      category: val.category,
-      type: val.type,
-      value: Number(val.value),
-      monthRegistered: format(val.dt, "MMMM"),
-      yearRegistered: format(val.dt, "yyyy"),
+
+      const totalExpenseReduced = transactionsFormatted.reduce((total, transaction) => {
+
+        if (transaction.type == "Expense" && transaction.yearRegistered == currentYear && transaction.monthRegistered == currentMonth) {
+          total += transaction.value;
+        }
+        return total;
+      }, 0);
+
+      const totalIncomeReduced = transactionsFormatted.reduce((total, transaction) => {
+
+        if (transaction.type == "Income" && transaction.yearRegistered == currentYear && transaction.monthRegistered == currentMonth) {
+          total += transaction.value;
+        }
+        return total;
+      }, 0);
+
+      setTotalExpense(totalExpenseReduced.toFixed(2));
+      setTotalIncome(totalIncomeReduced.toFixed(2));
+
+
+      setTotalBalance({
+        value: (Math.round((totalIncomeReduced - totalExpenseReduced) * 100) / 100).toFixed(2),
+        type: totalExpenseReduced > totalIncomeReduced ? "negative" : "positive",
+      })
+
     }
-  ))
+  }, [transactions])
 
-  const totalExpenseReduced = transactionsFormatted?.reduce((total, transaction) => {
-
-    if (transaction.type == "Expense" && transaction.yearRegistered == currentYear && transaction.monthRegistered == currentMonth) {
-      total += transaction.value;
-    }
-    return total;
-  }, 0);
-
-  const totalIncomeReduced = transactionsFormatted?.reduce((total, transaction) => {
-
-    if (transaction.type == "Income" && transaction.yearRegistered == currentYear && transaction.monthRegistered == currentMonth) {
-      total += transaction.value;
-    }
-    return total;
-  }, 0);
 
 
   return (
@@ -64,16 +81,16 @@ export function MonthBalanceCard({ user, transactions }: MonthBalanceCardProps) 
       <Flex align="center" >
         <Text fontWeight="medium" fontSize={30} color="#5FF099" >R$</Text>
         <Spacer />
-        <Skeleton isLoaded={!!transactionsFormatted} >
-          {transactions ? (<Text fontWeight="medium" fontSize={30} color="#5FF099" >{totalIncomeReduced.toFixed(2)}</Text>) : (<Container>0</Container>)}
+        <Skeleton isLoaded={!!transactions} >
+          {totalIncome ? (<Text fontWeight="medium" fontSize={30} color="#5FF099" >{totalIncome}</Text>) : (<Container>0</Container>)}
         </Skeleton>
       </Flex>
 
       <Flex align="center" >
         <Text fontWeight="medium" fontSize={30} color="#EB4335" >R$</Text>
         <Spacer />
-        <Skeleton isLoaded={!!transactionsFormatted} >
-          {transactions ? (<Text fontWeight="medium" fontSize={30} color="#EB4335" >-{totalExpenseReduced.toFixed(2)}</Text>) : (<Container>0</Container>)}
+        <Skeleton isLoaded={!!transactions} >
+          {totalExpense ? (<Text fontWeight="medium" fontSize={30} color="#EB4335" >-{totalExpense}</Text>) : (<Container>0</Container>)}
         </Skeleton>
       </Flex>
 
@@ -83,8 +100,8 @@ export function MonthBalanceCard({ user, transactions }: MonthBalanceCardProps) 
         <Text fontWeight="medium" fontSize={30} color="#5FF099" >R$</Text>
         <Spacer />
         <Skeleton isLoaded={!!transactions} >
-          <Text fontWeight="medium" fontSize={30} color={totalExpenseReduced > totalIncomeReduced ? "#EB4335" : "#5FF099"} >
-            {(Math.round((totalIncomeReduced - totalExpenseReduced) * 100) / 100).toFixed(2)}
+          <Text fontWeight="medium" fontSize={30} color={totalBalance?.type == "negative" ? "#EB4335" : "#5FF099"} >
+            {totalBalance?.value}
           </Text>
         </Skeleton>
       </Flex>
