7b46ecdd424ff755d8a53263968b861a41d0ffc0
NakajimaDaniel <nakajimadaniel96@gmail.com>
2022-04-12 19:16:31 -0300

    fix: types fix and prevent rollback to front page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,4 @@
 import { Container, HStack, VStack } from "@chakra-ui/react";
-import { useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { LatestExpensesCard } from "../../components/LatestExpensesCard";
@@ -18,11 +17,14 @@ interface Transaction {
   value: string,
 }
 
+interface TransactionDbResponse {
+  [key: string]: Transaction
+}
+
 export default function Dashboard() {
 
   const { user } = useContext(AuthContext);
   const { width } = useDimensions();
-  const router = useRouter();
 
   const [listOfTransactions, setListOfTransactions] = useState<Transaction[]>();
 
@@ -34,15 +36,12 @@ export default function Dashboard() {
       get(child(dbRef, `users/${user.uid}/transactions`)).then((snapshot) => {
         if (snapshot.exists()) {
           const data = snapshot.val();
-          const result = Object.entries(data).map(([key, value]) => { return value });
+          const result = Object.entries(data as TransactionDbResponse).map(([key, value]) => { return value });
           setListOfTransactions(result);
         }
       })
-    } else {
-      router.push('/')
     }
-
-  }, [])
+  }, [user]);
 
   if (user) {
     return (
@@ -50,12 +49,12 @@ export default function Dashboard() {
         <Header />
         {width && width > 660 ? (
           <HStack mr={width && width > 1180 ? ["200px", "200px", "250px", "300px"] : [0]} ml={width && width > 1180 ? ["200px", "200px", "250px", "300px"] : [0]} pb={10} alignItems="center">
-            <MonthBalanceCard user={user} transactions={listOfTransactions} />
+            <MonthBalanceCard transactions={listOfTransactions} />
             <TotalExpenseByCategoryCard transactions={listOfTransactions} />
           </HStack>
         ) : (
           <VStack mb={5}>
-            <MonthBalanceCard user={user} transactions={listOfTransactions} />
+            <MonthBalanceCard transactions={listOfTransactions} />
             <TotalExpenseByCategoryCard transactions={listOfTransactions} />
           </VStack>
         )}
